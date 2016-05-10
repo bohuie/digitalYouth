d24166a49e9693e91e2f9b758dd4a73c5870bc7d
Rodney Earl <rsearl.90@gmail.com>
2016-05-10 14:23:30 -0700

    Add user types to db seed then run rake to populate DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+[:admin, :employee, :employer].each do |role|
+  Role.create({ name: role })
+end

e0d4f836e00fe3c8aee54a504530782f8f1b4324
Rodney Earl <rsearl.90@gmail.com>
2016-05-10 14:25:38 -0700

    Default all actions to be done only by an admin (Authority functionality)

--- app/authorizers/application_authorizer.rb
+++ app/authorizers/application_authorizer.rb
@@ -8,9 +8,7 @@ class ApplicationAuthorizer < Authority::Authorizer
   # @param [Object] user - whatever represents the current user in your app
   # @return [Boolean]
   def self.default(adjective, user)
-    # 'Whitelist' strategy for security: anything not explicitly allowed is
-    # considered forbidden.
-    false
+    user.has_role? :admin
   end
 
 end

b1398bbaedeb3c5edbdf28cdb789226db6091bba
Bronson Bouchard <bouchardbronson@gmail.com>
2016-05-17 12:56:56 -0700

    Added fields to references
    Added some data fields to references and worked on the controller a bit

--- app/controllers/references_controller.rb
+++ app/controllers/references_controller.rb
@@ -1,2 +1,18 @@
 class ReferencesController < ApplicationController
+
+def show
+		@user = User.find(params[:id])
+
+		if @user.has_role? :employee
+			@references = @user.references;
+
+			if user_signed_in? && current_user.id == @user.id
+				##If user is signed in and the reference is the				
+			end
+
+	end
+end
+
+
+
 end
--- db/migrate/20160517193345_create_references.rb
+++ db/migrate/20160517193345_create_references.rb
@@ -2,6 +2,14 @@ class CreateReferences < ActiveRecord::Migration
   def change
     create_table :references do |t|
 
+      t.string  :first_name
+      t.string  :last_name
+      t.string	:email
+      t.string	:company
+      t.string	:position
+      t.text	:reference_body
+      t.integer :phone_number
+
       t.timestamps null: false
     end
   end

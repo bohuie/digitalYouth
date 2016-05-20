350b5cdd6d1201d06ec8871c27f5ae89e330def4
Bronson Bouchard <bouchardbronson@gmail.com>
2016-05-19 17:07:12 -0700

    references_controller test
    tests for get and show partially working

--- spec/controllers/references_controller_spec.rb
+++ spec/controllers/references_controller_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe ProjectsController, type: :controller do 
+RSpec.describe ReferencesController, type: :controller do 
 
 	describe "GET show" do
-
 		let(:reference1) { FactoryGirl.create(:reference1) }
-		let(:reference2) { FactoryGirl.create(:reference2) }
 
 		it "loads the specified reference" do
 			get :show, id: reference1.id
-
-			expect(assigns(:reference1)).to eq(reference1)
+			byebug
+			expect(assigns(:unconfirmed_references)).to eq(reference1)
 		end
+	end
+
+	describe "GET new" do
+
+		let(:reference1) { FactoryGirl.create(:reference1) }
+
+		it "redirects to root if the phrase is not in the db" do
+			get :new, id:"phrase"
+
+			expect(response).to redirect_to(root_path)
+		end
+
 	end
 
 end

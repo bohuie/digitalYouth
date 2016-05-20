0751496a13a4f1fa85627ccd5c1fea62e99f8932
Bronson Bouchard <bouchardbronson@gmail.com>
2016-05-20 12:46:04 -0700

    Reference controller testing
    wrote the tests for the delete and update actions, both currently fail. WIll figure it out on tuesday

--- spec/controllers/references_controller_spec.rb
+++ spec/controllers/references_controller_spec.rb
@@ -31,6 +31,27 @@ RSpec.describe ReferencesController, type: :controller do
 		end
 	end
 
+	describe "GET new" do
+
+		let(:reference_redirection1) { FactoryGirl.create(:reference_redirection1) }
+		let(:user) { FactoryGirl.create(:user) }
+
+		it "redirects to root if the phrase is not in the db" do
+			get :new, id:"phrase"
+
+			expect(response).to redirect_to(root_path)
+		end
+
+		it "loads the page if the phrase is in the db" do
+
+			user.reference_redirections << reference_redirection1
+			get :new, id: reference_redirection1.reference_url
+
+			expect(response).to render_template(:new)
+		end
+	end
+
+
 	describe "GET email" do
 
 		let(:user) { FactoryGirl.create(:user) }
@@ -50,9 +71,8 @@ RSpec.describe ReferencesController, type: :controller do
 		end
 	end
 
-
 	#this test is failing
-	describe "GET sendMail" do
+	describe "POST sendMail" do
 
 		let(:reference_email) { FactoryGirl.create(:reference_email1) }
 		let(:user) { FactoryGirl.create(:user) }
@@ -65,30 +85,76 @@ RSpec.describe ReferencesController, type: :controller do
 
 			expect{ReferenceMailer.reference_email(reference_email_params, :user).deliver_now}.to change{ActionMailer::Base.deliveries.count}.by(1)
 		end
-
 	end
 
+	describe "POST create" do
 
+		it "creates the reference" do
+			
+		end
 
-	describe "GET new" do
+		it "redirects to the root page if sucessful with a confirmation msg" do
+			
+		end
 
-		let(:reference_redirection1) { FactoryGirl.create(:reference_redirection1) }
+		it "redirects to the root page if unsucessful with a error msg" do
+			
+		end
+	end
+
+	#I cant figure out why the commented parts error when uncommented
+	describe "PATCH update" do
+		let(:reference1) { FactoryGirl.create(:reference1) }
 		let(:user) { FactoryGirl.create(:user) }
+		let(:user2) { FactoryGirl.create(:user2) }
 
-		it "redirects to root if the phrase is not in the db" do
-			get :new, id:"phrase"
-
-			expect(response).to redirect_to(root_path)
+		it "redirects the user when not logged in" do
+			patch :update#, :id reference1.id
+			byebug
+	
+			expect(response).to redirect_to(new_user_session_path)
 		end
 
-		it "loads the page if the phrase is in the db" do
+		it "redirects if user is not the reference's owner" do
+			user.references << reference1
+			sign_in user2
 
-			user.reference_redirections << reference_redirection1
-			get :new, id: reference_redirection1.reference_url
+			patch :update#, id: reference1.id
 
-			expect(response).to render_template(:new)
+			expect(response).to redirect_to(references_path)
 		end
 
+		it "updates the reference to set confirmed to the opposite" do
+			patch :update#, :id reference1.id
+
+		end
+	end
+
+	#I cant figure out why the commented parts error when uncommented
+	describe "DELETE delete" do
+		let(:reference1) { FactoryGirl.create(:reference1) }
+		let(:user) { FactoryGirl.create(:user) }
+		let(:user2) { FactoryGirl.create(:user2) }
+
+		it "redirects the user when not logged in" do
+			delete :delete#, :id reference1.id
+
+			expect(response).to redirect_to(new_user_session_path)
+		end
+
+		it "redirects if user is not the reference's owner" do
+			user.references << reference1
+			sign_in user2
+
+			delete :delete#, id: reference1.id
+
+			expect(response).to redirect_to(references_path)
+		end
+
+		it "deletes the reference" do
+			delete :delete#, :id reference1.id
+
+		end
 	end
 
 end

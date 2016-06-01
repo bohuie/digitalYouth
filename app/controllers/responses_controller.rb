289077c9d3c306f5a63d3c3e438bfa7c6236d5f3
Bronson Bouchard <bouchardbronson@gmail.com>
2016-06-01 10:38:29 -0700

    Response saving checks
    Added checks to make sure the responses properly save

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -5,30 +5,35 @@ class ResponsesController < ApplicationController
 	before_action :check_role
 
 	def create
+		# Fetch data from params
 		@user = current_user
 		@survey_id = Integer(params[:response][:survey_id])
 		@scores = params[:response][:scores]
 		@question_ids = params[:response][:question_ids]
 
-		# Prevents users from entering duplicates when hitting back after creating a response
+		# Check to see if the survey is unanswered for the user and make a new response if its unanswered
 		if @user.answered_surveys[@survey_id - 1] == false
 			@response = Response.new(user_id: @user.id, survey_id: @survey_id)
-		else
+		else # Prevent users from entering duplicates when hitting back after creating a response
 			@response = Response.find_by(user_id: @user.id, survey_id: @survey_id)
 		end
 
 		#Record response to database
 		@response.set_data_from_hash(@scores,@question_ids)
-		@response.save
-
+		if !@response.save
+			redirect_to current_user, flash: {danger: "There was an issue saving your response"}
+		end
 		#Record that the user has answered the survey
 		@user.answered_surveys[@survey_id - 1] = true
-		@user.save
-
+		if !@user.save
+			@response.destroy # The survey wasn't marked properly in the user table, response is destroyed.
+			redirect_to current_user, flash: {danger: "There was an issue saving your response"}
+		end
 		redirect_to current_user, flash: {success: "Thanks for answering the survey!"}
 	end
 
 	def update
+		# Fetch data from params
 		@user = current_user
 		@survey_id = Integer(params[:response][:survey_id])
 		@scores = params[:response][:scores]
@@ -37,8 +42,9 @@ class ResponsesController < ApplicationController
 		# Update the record
 		@response = Response.find_by(user_id: @user.id, survey_id: @survey_id)
 		@response.set_data_from_hash(@scores,@question_ids)
-		@response.save
-
+		if !@response.save
+			redirect_to current_user, flash: {danger: "There was an issue saving your response"}
+		end
 		redirect_to current_user, flash: {success: "Thanks for answering the survey!"}
 	end
 
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,9 +1,10 @@
 class Response < ActiveRecord::Base
 	belongs_to :user
+
 	# The connection between a response and the questions is in the question_ids
 	# scores[i] is a response to question_ids[i] question
 
-	#Creates a Hashmap for chartkick to display {"classification" => score}
+	# Creates a Hashmap for chartkick to display {"classification" => score}
 	def get_data_map
 		length = self.scores.size
 		rtn = Hash.new

34b189266d8233a986b5358f87252848c691c0fa
Bronson Bouchard <bouchardbronson@gmail.com>
2016-06-01 16:33:36 -0700

    Trying to implement notifications
    attempt at notifications

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -1,5 +1,15 @@
 class Reference < ActiveRecord::Base
-
 	belongs_to :user
-	
+
+	include StreamRails::Activity
+	as_activity
+
+	def activity_notify
+		[StreamRails.feed_manager.get_notification_feed(self.user_id)]
+	end
+
+	def activity_object
+		self
+	end
+
 end
--- config/initializers/stream_rails.rb
+++ config/initializers/stream_rails.rb
@@ -8,10 +8,5 @@ StreamRails.configure do |config|
   config.api_secret  = "xbas8tj7a6tc8znq9a9h4ajjezhznqfw4peezbvg6e8gzf4cq6fwvbgpqmxj3xq8"
   config.timeout     = 30               
   config.location    = 'us-west'        
-  # If you use custom feed names, e.g.: timeline_flat, timeline_aggregated,
-  # use this, otherwise omit:
-  config.news_feeds = { flat: "timeline_flat", aggregated: "timeline_aggregated" }
-  # Point to the notifications feed group providing the name, omit if you don't
-  # have a notifications feed
   config.notification_feed = "notifications"
 end

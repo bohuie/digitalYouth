18d00d6b696ea5a7350962c5c8aeb3ab0341d293
Rodney Earl <rsearl.90@gmail.com>
2016-06-23 13:34:36 -0700

    Added action mailer's address to .yml files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: ENV['EMAIL_ADDRESS'],
   #domain: ENV['EMAIL_DOMAIN'],
   port: 587,
   authentication: "plain",
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: ENV['EMAIL_ADDRESS'],
   port: 587,
   authentication: "plain",
   enable_starttls_auto: true,

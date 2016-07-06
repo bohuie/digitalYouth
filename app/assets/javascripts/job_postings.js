869ee1e4f177ed4184cae7f68942da3be4fcbaa5
Bronson Bouchard <bouchardbronson@gmail.com>
2016-07-06 11:03:46 -0700

    Job Posting Form - Adding Skills
    Added a javascript way to add skill fields to the Job Posting form

--- app/assets/javascripts/job_postings.js
+++ app/assets/javascripts/job_postings.js
@@ -10,19 +10,28 @@ function remove_posting_skill(id){
 
 var cache = {};
 function skill_autocomplete(id){
+  console.log("AutoComplete");
 	$("#skill-name-"+id).autocomplete({
-		minLength: 2,
-      	source: function( request, response ) {
+		    minLength: 2,
+      	source: function(request, response){
         var term = request.term;
-        if ( term in cache ) {
-          response( cache[ term ] );
+        if (term in cache){
+          response(cache[term]);
           return;
         }
  
-        $.getJSON( '/job_postings/skill_autocomplete.json', request, function( data, status, xhr ) {
-          cache[ term ] = data;
-          response( data );
-        });
+        $.getJSON('/job_postings/skill_autocomplete.json', request, function(data, status, xhr){
+          cache[term] = data;
+          response(data);
+        }); 
     }
   });
+}
+
+function add_fields(association, content){
+  var skills = 0;
+  $('#job-skills').children().each(function(){if(this.id.includes("job-posting-skill-")) skills++;});
+  var new_id = skills + 1;
+  var regexp = new RegExp("new_" + association, "g");
+  $('#job-skills').append(content.replace(regexp, new_id));
 }

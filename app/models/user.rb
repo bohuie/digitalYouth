8a4d3be76a52692f341e6877a35c1b7634e77749
Rodney Earl <rsearl.90@gmail.com>
2016-09-13 10:16:58 -0700

    Code cleanup and New User
    Cleaned up some views.  Removed the code that creates a user if they
    cannot be found.  Instead, we redirect to the signup page with a
    message.

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,23 +46,10 @@ class User < ActiveRecord::Base
         if user.nil?
 
             # Get the existing user by email if the provider gives us a verified email.
-            # If no verified email was provided we assign a temporary email and ask the
-            # user to verify it on the next step via UsersController.finish_signup
+            # If no verified email was provided we ask the user to login or signup
             email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
             email = auth.info.email if email_is_verified
             user = User.where(:email => email).first if email
-
-            # Create the user if it's a new registration
-            if user.nil?
-                user = User.new(
-                    name: auth.extra.raw_info.name,
-                    #username: auth.info.nickname || auth.uid,
-                    email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
-                    password: Devise.friendly_token[0,20]
-                )
-                user.skip_confirmation!
-                user.save!
-            end
         end
 
         # Associate the identity with the user if needed

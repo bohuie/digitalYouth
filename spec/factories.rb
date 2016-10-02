d87650e51e44fbbfae9c467bafbf787ad99ab713
Bronson Bouchard <bouchardbronson@gmail.com>
2016-10-01 19:53:55 -0700

    Testing Fixes
    Fixed some rspec tests.
    Many tests are failing now, they should be addressed.

--- spec/factories.rb
+++ spec/factories.rb
@@ -30,10 +30,10 @@ FactoryGirl.define do
     password  "password"
     password_confirmation "password"
     company_name "Yahoo"
-    company_address "123 Fake Street"
-    company_city "Kelowna"
-    company_province "BC"
-    company_postal_code "V1V 1V1"
+    street_address "123 Fake Street"
+    city "Kelowna"
+    province "BC"
+    postal_code "V1V 1V1"
   end
 
   factory :employer2, class: User do
@@ -43,10 +43,10 @@ FactoryGirl.define do
     password  "password"
     password_confirmation "password"
     company_name "Google"
-    company_address "321 Fake Street"
-    company_city "Penticton"
-    company_province "BC"
-    company_postal_code "V7V 7X7"
+    street_address "321 Fake Street"
+    city "Penticton"
+    province "BC"
+    postal_code "V7V 7X7"
   end
 
   factory :project do

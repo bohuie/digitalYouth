eda09780c1b9ef352f3ea5cb9319010511fce939
Rodney Earl <rsearl.90@gmail.com>
2016-11-07 11:29:10 -0800

    Updated new project page and model
    Added header to the project new page.  Updated project model for cases
    when a project has no skills (fringe), and then the view

--- app/models/project.rb
+++ app/models/project.rb
@@ -28,32 +28,37 @@ class Project < ActiveRecord::Base
 	end
 
 	def process_skills(hash) # Creates and Updates project skills, creating new skills when needed.
-		hash.each do |m|
-			id = m[1]["id"]
-			if m[1]["_destroy"] == "true"
-				ProjectSkill.find(id).destroy if !id.blank?
-			elsif m[1]["_destroy"] == "false" || m[1]["_destroy"].empty?
-				if m[1]["skill"].nil?
-					skill_name = m[1]["skill_attributes"]["name"].downcase
-				else
-					skill_name = m[1]["skill"].downcase
-				end
-				skill = Skill.find_by(name: skill_name)
-				if skill.nil?
-					skill = Skill.new(name: skill_name)
-					return false if !skill.save
-				end
-				skill_id = skill.id
-				survey_id = m[1]["survey_id"]
-				if id.blank?
-					project_skill = ProjectSkill.new(skill_id: skill_id, survey_id: survey_id, project_id: self.id)
-					return false if !project_skill.save
-				else
-					project_skill = ProjectSkill.find(id)
-					return false if !project_skill.update(id: project_skill.id, skill_id: skill_id, survey_id: survey_id, project_id: self.id)
+		byebug
+		if hash.nil?
+			return true
+		else
+			hash.each do |m|
+				id = m[1]["id"]
+				if m[1]["_destroy"] == "true"
+					ProjectSkill.find(id).destroy if !id.blank?
+				elsif m[1]["_destroy"] == "false" || m[1]["_destroy"].empty?
+					if m[1]["skill"].nil?
+						skill_name = m[1]["skill_attributes"]["name"].downcase
+					else
+						skill_name = m[1]["skill"].downcase
+					end
+					skill = Skill.find_by(name: skill_name)
+					if skill.nil?
+						skill = Skill.new(name: skill_name)
+						return false if !skill.save
+					end
+					skill_id = skill.id
+					survey_id = m[1]["survey_id"]
+					if id.blank?
+						project_skill = ProjectSkill.new(skill_id: skill_id, survey_id: survey_id, project_id: self.id)
+						return false if !project_skill.save
+					else
+						project_skill = ProjectSkill.find(id)
+						return false if !project_skill.update(id: project_skill.id, skill_id: skill_id, survey_id: survey_id, project_id: self.id)
+					end
 				end
 			end
+			return true
 		end
-		return true
 	end
 end

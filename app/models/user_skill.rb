b5c49178239beb45b7728aabc1a183cae4c16e4b
Rodney Earl <rsearl.90@gmail.com>
2016-11-16 10:02:47 -0800

    Add project skills to user skills
    Include uniqueness of skill/survey when processes project skills for a
    user

--- app/models/user_skill.rb
+++ app/models/user_skill.rb
@@ -6,5 +6,7 @@ class UserSkill < ActiveRecord::Base
 	belongs_to :skill
 	belongs_to :survey
 
+	validates_uniqueness_of :user_id, scope: [:skill_id, :survey_id]
+
 	#validates :rating, presence: true, rating: true
 end

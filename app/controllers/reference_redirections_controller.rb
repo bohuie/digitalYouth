38afa4867de485542c343d81a247b7aabbc374ff
Rodney Earl <rsearl.90@gmail.com>
2017-01-09 13:09:37 -0800

    Bane of my existence
    Bye bug

--- app/controllers/reference_redirections_controller.rb
+++ app/controllers/reference_redirections_controller.rb
@@ -1,7 +1,6 @@
 class ReferenceRedirectionsController < ApplicationController
 
 	def destroy # Deletes a reference request
-		byebug
 		request = ReferenceRedirection.find(params[:id])
 		request.destroy
 		redirect_to current_user, flash: {success: "Reference request removed."}

c6762e5b77655672983267c72c0bc22af3c10553
Rodney Earl <rsearl.90@gmail.com>
2017-02-23 11:21:31 -0800

    Migrations for Ideal Candidate Survey

---
+++ db/migrate/20170223190652_add_answered_surveys_to_job_posting.rb
@@ -0,0 +1,5 @@
+class AddAnsweredSurveysToJobPosting < ActiveRecord::Migration
+  def change
+    add_column :job_postings, :answered_survey, :boolean, array: true, default: [false, false, false, false, false, false, false, false, false, false, false, false]
+  end
+end

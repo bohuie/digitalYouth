58717553e458fbbac8fa8c10277b00b71d5b3653
Rodney Earl <rsearl.90@gmail.com>
2017-03-22 11:41:46 -0700

    Survey Links

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -6,8 +6,8 @@ JOB_TYPES = {"Full Time" => 0,"Part Time" => 1,"Contract" => 2,"Casual" => 3,
 
 PAY_RATES = {"Yearly" => "yearly", "Hourly" => "hourly"}.freeze
 
-SURVEYS = { 0 => "General Computer", 1 => "Internet and Networks", 2 => "Programming", 3 => "Word Processing", 
-			4 => "Spreadsheet", 5 => "Online Communication and Collaboration", 6 => " Time, Proejct, People", 
+SURVEYS = { 0 => "General Computing", 1 => "Internet and Networks", 2 => "Programming", 3 => "Word Processing", 
+			4 => "Spreadsheet", 5 => "Online Communication and Collaboration", 6 => "Time, Project, People", 
 			7 => "Money", 8 => "Presentation", 9 => " Multimedia", 10 => "Social Media", 11 => "21st Century Skills" }.freeze
 
 GENDERS = { "Male" => "male", "Female" => "female"}.freeze

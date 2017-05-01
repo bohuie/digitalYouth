fbddfdb8f54dee4965f57908b0193891a0e45915
Rodney Earl <rsearl.90@gmail.com>
2017-05-01 14:32:26 -0700

    Try concatenating

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :branch, "admin"
 #set :branch, ENV.fetch('REVISION', 'admin')
 
 set :linked_files, %w(config/application.yml)
-set :linked_dir, %w{public/system}
+set :linked_dir, fetch(:linked_dirs) +  %w{public/system}
 
 set :stages, "production"
 
